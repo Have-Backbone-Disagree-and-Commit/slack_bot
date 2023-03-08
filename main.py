b43dc3551f3ef810b3122e891143aec9fec460c2
Arc1el <khm970514@gmail.com>
2023-03-08 11:18:22 +0900

    migration fastapi app

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from .routers import crawlRouter
 from mangum import Mangum
 
 app = FastAPI()
+app.include_router(crawlRouter.router)
 handler = Mangum(app)
 
 @app.get("/")
